7b369803d13a818c343f434cabe015d92c77f06a
unknown <draz@ualberta.ca>
2020-08-17 08:54:37 -0600

    card models

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,6 +10,17 @@ const cards = new Schema({
   Type: String,
 });
 
+const myCollection = new Schema({
+  name: String,
+  id: String,
+  level: Number,
+  atk: Number,
+  def: Number,
+  Type: String,
+  cards: [cards],
+});
+
 //Type vs Race
 
 module.exports = mongoose.model("Cards", cards);
+module.exports = mongoose.model("MyCollection", myCollection);
