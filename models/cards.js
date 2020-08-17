e69191268a1ccf7d03c49dc7960e11442f7d1acf
unknown <draz@ualberta.ca>
2020-08-17 08:56:15 -0600

    switches

---
+++ models/cards.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cards = new Schema({
+  name: String,
+  id: String,
+  level: Number,
+  atk: Number,
+  def: Number,
+  Type: String,
+});
+
+const myCollection = new Schema({
+  name: String,
+  id: String,
+  level: Number,
+  atk: Number,
+  def: Number,
+  Type: String,
+  cards: [cards],
+});
+
+//Type vs Race
+
+module.exports = mongoose.model("Cards", cards);
+module.exports = mongoose.model("MyCollection", myCollection);
