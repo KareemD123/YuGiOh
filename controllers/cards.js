6c0047fbd200a95fb4084ad9f3fe7b5e97189fca
unknown <draz@ualberta.ca>
2020-08-17 10:01:52 -0600

    cardforeach

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,15 +1,22 @@
 const Cards = require("../models/cards");
+const request = require("request");
 
 module.exports = {
   requestApi,
 };
 
 function requestApi() {
-  request(
-    "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician",
-    function (err, response, body) {
-      console.log(response.json);
-      res.render("index", {});
-    }
-  );
+  request("https://db.ygoprodeck.com/api/v7/cardinfo.php", function (
+    err,
+    response,
+    body
+  ) {
+    // console.log(JSON.parse(response));
+    let res2 = JSON.parse(response.body);
+    let res3 = res2.data;
+    res3.forEach(function (name) {
+      console.log(name.name);
+    });
+    // console.log(res2.data);
+  });
 }
