7a79fb7756577f2e30fccc56a1307cc757903f78
unknown <draz@ualberta.ca>
2020-08-18 09:34:13 -0600

    enter

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,7 +10,6 @@ function requestApi(req, res) {
   request(
     "https://db.ygoprodeck.com/api/v7/cardinfo.php?level=4&attribute=water&sort=atk",
     function (err, req, body) {
-      // if (err) return console.log(err);
       let req2 = JSON.parse(req.body);
       let req3 = req2.data;
       // console.log(req3);
@@ -52,10 +51,10 @@ function requestApi(req, res) {
 // console.log(req2.data);
 // console.log(name.name);
 
-function showCards(req, res) {
+function showCards(err, req, res) {
   console.log("hello2");
+
   Cards.find({}, function (err, cards) {
-    // if (err) return err;
     // console.log(cards);
     res.render("show.ejs", { cards });
   });
